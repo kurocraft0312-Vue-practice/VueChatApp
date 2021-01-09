41c2c76cc274a3a513638180590a14efd6604633
kurocraft0312 <kurocraft0312@gmail.com>
2021-01-09 23:03:30 +0900

    add:chatboardコンポーネント作成

--- vue-chat/src/App.vue
+++ vue-chat/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <Header />
+    <ChatBoard />
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import ChatBoard from './components/ChatBoard'
 export default {
   name: 'App',
 
   components: {
-    Header
+    Header,
+    ChatBoard
   },
 
   data: () => ({
